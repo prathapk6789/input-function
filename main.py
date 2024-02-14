7c87557a956d3389614a419c3a086f9c696fa841
k.Prathap <159271537+prathapk6789@users.noreply.github.com>
2024-02-14 18:37:49 +0530

    input functuions

---
+++ main.py
@@ -0,0 +1,26 @@
+a=int(input("addition of :"))
+b=int(input("addition of :"))
+print(f"addition of {a}+{b}={a+b}")
+
+a=int(input("subtraction of :"))
+b=int(input("subtraction of :"))
+print(f"subtraction of {a}-{b}={a-b}")
+
+a=int(input("division of :"))
+b=int(input("division of :"))
+print(f"division of {a}/{b}={a/b}")
+
+a=int(input("multiplication of :"))
+b=int(input("mumtiplication of :"))
+print(f"multiplication of {a}*{b}={a*b}")
+
+a=int(input("module of :"))
+b=int(input("module of :"))
+print(f"module of {a}%{b}={a%b}")
+
+a=int(input("floor_division of :"))
+b=int(input("floor_division of :"))
+print(f"floor_division of {a}//{b}={a//b}")
+
+
+
